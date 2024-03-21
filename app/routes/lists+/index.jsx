58e20256eabcf99af3e5e47f60e149ff0d95d171
ajccarlson <32660587+ajccarlson@users.noreply.github.com>
2024-03-21 14:34:19 -0700

    Modularized grid code

--- app/routes/lists+/index.jsx
+++ app/routes/lists+/index.jsx
@@ -1,12 +1,14 @@
 import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model'
 import { ModuleRegistry } from '@ag-grid-community/core'
 import { AgGridReact } from '@ag-grid-community/react'
-import listThumbnailRenderer from "#app/utils/list-thumbnail-renderer.tsx";
-import { scoreColor, scoreRange } from "#app/utils/list-score-colorer.tsx";
 import { useFetcher } from '@remix-run/react'
 import '@ag-grid-community/styles/ag-grid.css';
 import "#app/styles/watchlist.scss";
 import { useCallback, useEffect, useState } from 'react'
+import listThumbnailRenderer from "#app/utils/lists/thumbnail-renderer.tsx";
+import { scoreColor, scoreRange } from "#app/utils/lists/score-colorer.tsx";
+import { differenceFormatter } from "#app/utils/lists/column-functions.tsx";
+import { rowData } from "#app/utils/lists/test-data.tsx";
 
     
 ModuleRegistry.registerModules([ ClientSideRowModelModule ]);
@@ -64,121 +66,9 @@ const columnDefs = [
   { field: 'Enjoyment', flex: 1, minWidth: 52, maxWidth: 80, cellEditor: 'agNumberCellEditor', cellEditorParams: { min: 1, max: 10, precision: 1, step: 1, showStepperButtons: true }, cellClass: "ag-score-cell", cellStyle: function(params) { let scoreType = "Default"; return scoreColor( { range: scoreRange(), score: params.value, type: scoreType } ) } },
   { field: 'Averaged', flex: 1, minWidth: 62, maxWidth: 90, editable: false, cellClass: "ag-score-border-left-double ag-score-cell", cellStyle: function(params) { let scoreType = "Default"; return scoreColor( { range: scoreRange(), score: params.value, type: scoreType } ) } },
   { field: 'Personal', flex: 1, minWidth: 55, maxWidth: 80, cellEditor: 'agNumberCellEditor', cellEditorParams: { min: 1, max: 10, precision: 1, step: 0.1, showStepperButtons: true }, cellClass: "ag-score-cell", cellStyle: function(params) { let scoreType = "Default"; return scoreColor( { range: scoreRange(), score: params.value, type: scoreType } ) } },
-  { field: 'Difference_Personal', headerName: 'Difference: Personal', flex: 1, minWidth: 70, maxWidth: 90, editable: false, cellClass: "ag-score-cell", cellStyle: function(params) { let scoreType = "Difference Personal"; return  scoreColor( { range: scoreRange(scoreType), score: params.value, type: scoreType } ) } },
+  { field: 'Difference_Personal', headerName: 'Difference: Personal', valueGetter: 'data.Personal - data.Averaged', valueFormatter: params => differenceFormatter(params), flex: 1, minWidth: 70, maxWidth: 90, editable: false, cellClass: "ag-score-cell", cellStyle: function(params) { let scoreType = "Difference Personal"; return  scoreColor( { range: scoreRange(scoreType), score: params.value, type: scoreType } ) } },
   { field: 'TMDB_Score', headerName: 'TMDB Score', flex: 1, minWidth: 55, maxWidth: 80, editable: false, cellClass: "ag-score-border-left-double ag-score-cell", cellStyle: function(params) { let scoreType = "TMDB Score"; return  scoreColor( { range: scoreRange(scoreType), score: params.value, type: scoreType } ) } },
-  { field: 'Difference_Objective', headerName: 'Difference: Objective', flex: 1, minWidth: 70, maxWidth: 90, editable: false, cellClass: "ag-score-cell", cellStyle: function(params) { let scoreType = "Difference Objective"; return  scoreColor( { range: scoreRange(scoreType), score: params.value, type: scoreType } ) } }
-]
-
-const rowData = [
-  { '#': '1',
-  'Thumbnail': 'Picture',
-  'Title': 'Before Sunset',
-  'Type': 'Movie',
-  'Air_Year': '2004',
-  'Length': '1h 20m',
-  'Rating': 'R',
-  'Finished_Date': '6/4/23',
-  'Genre(s)': 'Drama, Romance',
-  'Language': 'English',
-  'Story': '6',
-  'Character': '10',
-  'Presentation': '5',
-  'Sound': '7',
-  'Performance': '9',
-  'Enjoyment': '10',
-  'Averaged': '7.84',
-  'Personal': '10',
-  'Difference_Personal': '+2.16',
-  'TMDB_Score': '7.8',
-  'Difference_Objective': '+2.20' },
-
-  { '#': '2',
-  'Thumbnail': 'Picture',
-  'Title': 'The Godfather Part II',
-  'Type': 'Movie',
-  'Air_Year': '1974',
-  'Length': '3h 22m',
-  'Rating': 'R',
-  'Finished_Date': '4/23/23',
-  'Genre(s)': 'Crime, Drama',
-  'Language': 'English',
-  'Story': '7',
-  'Character': '8',
-  'Presentation': '7',
-  'Sound': '8',
-  'Performance': '7',
-  'Enjoyment': '6',
-  'Averaged': '7.17',
-  'Personal': '7.2',
-  'Difference_Personal': '+0.03',
-  'TMDB_Score': '8.6',
-  'Difference_Objective': '-1.4' },
-
-  { '#': '3',
-  'Thumbnail': 'Picture',
-  'Title': 'Our Little Sister',
-  'Type': 'Movie',
-  'Air_Year': '2015',
-  'Length': '2h 7m',
-  'Rating': 'PG',
-  'Finished_Date': '6/23/23',
-  'Genre(s)': 'Drama, Family',
-  'Language': 'Japanese',
-  'Story': '3',
-  'Character': '6',
-  'Presentation': '8',
-  'Sound': '8',
-  'Performance': '7',
-  'Enjoyment': '7',
-  'Averaged': '6.5',
-  'Personal': '5.4',
-  'Difference_Personal': '-1.10',
-  'TMDB_Score': '7.6',
-  'Difference_Objective': '-2.20' },
-
-  { '#': '4',
-  'Thumbnail': 'Picture',
-  'Title': 'Gen V',
-  'Type': 'TV Series*',
-  'Air_Year': '2023-',
-  'Length': '8 / 8 eps',
-  'Rating': 'TV-MA',
-  'Finished_Date': '7/20/23',
-  'Genre(s)': 'Action, Adventure, Comedy',
-  'Language': 'English',
-  'Story': '4',
-  'Character': '4',
-  'Presentation': '7',
-  'Sound': '6',
-  'Performance': '6',
-  'Enjoyment': '5',
-  'Averaged': '5.34',
-  'Personal': '3.4',
-  'Difference_Personal': '-2.34',
-  'TMDB_Score': '8',
-  'Difference_Objective': '-4.60' },
-
-  { '#': '5',
-  'Thumbnail': 'Picture',
-  'Title': 'Cunk on Earth',
-  'Type': 'TV Series',
-  'Air_Year': '2022-2022',
-  'Length': '10 / 10 eps',
-  'Rating': 'TV-MA',
-  'Finished_Date': '3/7/23',
-  'Genre(s)': 'Comedy',
-  'Language': 'English',
-  'Story': '1',
-  'Character': '1',
-  'Presentation': '6',
-  'Sound': '6',
-  'Performance': '3',
-  'Enjoyment': '2',
-  'Averaged': '3.17',
-  'Personal': '1.2',
-  'Difference_Personal': '-1.97',
-  'TMDB_Score': '7.8',
-  'Difference_Objective': '-6.6' }
+  { field: 'Difference_Objective', headerName: 'Difference: Objective', valueGetter: 'data.Personal - data.TMDB_Score', valueFormatter: params => differenceFormatter(params), flex: 1, minWidth: 70, maxWidth: 90, editable: false, cellClass: "ag-score-cell", cellStyle: function(params) { let scoreType = "Difference Objective"; return  scoreColor( { range: scoreRange(scoreType), score: params.value, type: scoreType } ) } }
 ]
 
 export default function Index() {
