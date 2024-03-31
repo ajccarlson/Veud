965e250f4bfb3595fb1733edc1b31873004e497d
ajccarlson <32660587+ajccarlson@users.noreply.github.com>
2024-03-30 23:53:20 -0700

    Changed how last updated info is display in lists landing page

--- app/routes/lists+/index.jsx
+++ app/routes/lists+/index.jsx
@@ -1,44 +1,38 @@
 import { json } from "@remix-run/node"
 import { useLoaderData } from '@remix-run/react'
 import { prisma } from '#app/utils/db.server.ts'
+import { timeSince } from "#app/utils/lists/column-functions.tsx"
 
 function getWatchlistNav(watchListData) {
   return (
-    // `<div> <div> ` +
-    // `Name: ${watchListData.watchlist.header}` +
-    // ` </div> <div>` +
-    // `Description: ${watchListData.watchlist.description}` +
-    // `</div> <div> ` +
-    // `Last Updated: ${watchListData.watchlist.updatedAt}` +
-    // `</div> <div>` +
-    // `Length: ${watchListData.listEntries.length}` +
-    // ` </div> </div>`
-
-
-    `<div class="grid grid-cols-[85%_15%] font-sansp-6 bg-[#464646] w-[55rem] font-[arial] border-8 border-[#383040] rounded-lg">` + 
-      `<div class="flex-auto">` + 
-        `<div class="flex flex-wrap border-b-[4px] bg-[#121212] border-[#66563d] border-spacing-y px-3 pt-1">` +
-          `<h1 class="flex-auto text-xl font-semibold text-[#ffffb1] pl-5">` + 
-            `${watchListData.watchlist.header}` +
-          `</h1>` + 
-          `<div class="text-lg font-semibold text-[#ccedff] pb-1">` + 
-            `${watchListData.listEntries.length}` +
-          `</div>` + 
-        `</div>` + 
-        `<p class="text-m text-white font-['Playfair_Display'] px-3 py-3 shadow-[0_35px_60px_-15px_rgba(46,47,43,18)]">` + 
-          `${watchListData.watchlist.description}` +
-        `</p>` +
-        `<div class="w-full flex-none text-xs font-semibold text-[#408063] mt-2 text-end px-3 pb-1">` + 
-          `Last Updated: ` + `<span class="text-[#dbffcc]">` + `${watchListData.watchlist.updatedAt}` + `</span>` +
+    `<div class="flex-auto font-sansp-6 bg-[#464646] w-[55rem] font-[arial] border-8 border-[#383040] rounded-lg">` + 
+      `<div class="flex flex-wrap border-b-[4px] bg-[#121212] border-[#66563d] border-spacing-y px-3 pt-1">` +
+        `<h1 class="flex-auto text-xl font-semibold text-[#ffffb1] pl-5">` + 
+          `${watchListData.watchlist.header}` +
+        `</h1>` + 
+        `<div class="text-lg font-semibold text-[#FF9900] pb-1">` + 
+          `${watchListData.listEntries.length}` +
         `</div>` + 
       `</div>` + 
-      `<div class="grid grid-rows-[85%_15%]">` + 
-        `<button class="font-semibold bg-[#6F6F6F] hover:bg-[#8CA99D] text-[#FFEFCC] /*border-8 border-[#383040]*/" type="submit">` + 
+
+      `<div class="grid grid-cols-[85%_15%] ">` + 
+        `<div>` + 
+          `<p class="text-m text-white font-['Playfair_Display'] px-3 py-3 shadow-[0_35px_60px_-15px_rgba(46,47,43,18)]">` + 
+            `${watchListData.watchlist.description}` +
+          `</p>` +
+          `<div class="w-full flex-none text-xs font-semibold text-[#408063] mt-2 text-end px-3 pb-1 pr-8">` + 
+            `Last Updated: ` + `<span class="text-[#dbffcc]">` + `${timeSince(watchListData.watchlist.updatedAt)}` + `</span>` +
+          `</div>` + 
+        `</div>` + 
+
+        `<div class="grid grid-rows-[85%_15%] /*border-8 border-[#383040]*/">` + 
+          `<a href=${"/lists/" + watchListData.watchlist.name} class="flex justify-center items-center text-center font-semibold bg-[#6F6F6F] hover:bg-[#8CA99D] text-[#FFEFCC]">` + 
             `Open` + 
-        `</button>` + 
-        `<button class="font-semibold bg-[#6F6F6F] hover:bg-[#8CA99D] text-[#FFEFCC] /*border-8 border-[#383040]*/" type="submit">` + 
-            `Settings` + 
-        `</button>` + 
+          `</a>` +  
+          `<button class="font-semibold bg-[#6F6F6F] hover:bg-[#8CA99D] text-[#FFEFCC]" type="submit">` + 
+              `Settings` + 
+          `</button>` + 
+        `</div>` + 
       `</div>` + 
     `</div>` + 
     `<br></<br>` 
