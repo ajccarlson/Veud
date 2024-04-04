f4a48e0b1d6873ced7db479cc3a9a5e36fa7e395
ajccarlson <32660587+ajccarlson@users.noreply.github.com>
2024-04-03 21:47:31 -0700

    Added comditional rendering of list type nav from watchlists

--- app/components/list-nav-buttons.jsx
+++ app/components/list-nav-buttons.jsx
@@ -46,47 +46,55 @@ export function listNavButtons(watchLists, username, listType, watchListData) {
     }
   }
 
-  return (
-    <div class="font-family: arial bg-[#464646] text-[#FFEFCC] border-t-8 border-t-[#54806C] pt-3 pb-1 shadow-[inset_0_-6px_8px_rgba(0,0,0,0.6)]" id="list-nav">
-      <div class="flex flex-col bg-[#6F6F6F] fixed float-left font-bold rounded" id="list-type-nav">
-        <div class="content-center text-center justify-center rounded">
-
-
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <div class="bg-[#6F6F6F] hover:bg-[#8CA99D] cursor-pointer text-base font-bold py-[0.1rem] px-[0.5rem] rounded"> 
-                {currentType.formatted}
-              </div>
-            </DropdownMenuTrigger>
-            <DropdownMenuPortal>
-              <DropdownMenuContent sideOffset={8} align="start">
-                {listTypes.map( typeMap =>
-                  <DropdownMenuItem>
-                    <Link to={"../lists/" + username + "/" + typeMap.name + "/"} /*onClick={(e) => this.columnsDifferent(e, watchListData, list)}*/
-                      class="bg-[#6F6F6F] hover:bg-[#8CA99D] text-base font-bold py-[0.1rem] px-[0.5rem] rounded"> 
-                        {typeMap.formatted}
-                    </Link>
-                  </DropdownMenuItem>
-                )}
-              </DropdownMenuContent>
-            </DropdownMenuPortal>
-          </DropdownMenu>
-
-
-
-
-          
-          
+  if (listTypes && listTypes.length > 0) {
+    return (
+      <div class="font-family: arial bg-[#464646] text-[#FFEFCC] border-t-8 border-t-[#54806C] pt-3 pb-1 shadow-[inset_0_-6px_8px_rgba(0,0,0,0.6)]" id="list-nav">
+        <div class="flex flex-col bg-[#6F6F6F] fixed float-left font-bold rounded" id="list-type-nav">
+          <div class="content-center text-center justify-center rounded">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <div class="bg-[#6F6F6F] hover:bg-[#8CA99D] cursor-pointer text-base font-bold py-[0.1rem] px-[0.5rem] rounded"> 
+                  {currentType.formatted}
+                </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuContent sideOffset={8} align="start">
+                  {listTypes.map( typeMap =>
+                    <DropdownMenuItem>
+                      <Link to={"../lists/" + username + "/" + typeMap.name + "/"} /*onClick={(e) => this.columnsDifferent(e, watchListData, list)}*/
+                        class="bg-[#6F6F6F] hover:bg-[#8CA99D] text-base font-bold py-[0.1rem] px-[0.5rem] rounded"> 
+                          {typeMap.formatted}
+                      </Link>
+                    </DropdownMenuItem>
+                  )}
+                </DropdownMenuContent>
+              </DropdownMenuPortal>
+            </DropdownMenu>
+          </div>
+        </div>
+        <div class="flex flex-row gap-4 justify-center">
+          {watchLists.map( list =>
+            <Link to={"../lists/" + username + "/" + listType + "/" + list.name} /*onClick={(e) => this.columnsDifferent(e, watchListData, list)}*/
+            class="bg-[#6F6F6F] hover:bg-[#8CA99D] text-base font-bold py-5 px-16 border-b-4 border-[#A2FFD5] hover:border-[#80FFC6] rounded"> 
+              {list.header}
+            </Link>
+          )}
         </div>
       </div>
-      <div class="flex flex-row gap-4 justify-center">
-        {watchLists.map( list =>
-          <Link to={"../lists/" + username + "/" + listType + "/" + list.name} /*onClick={(e) => this.columnsDifferent(e, watchListData, list)}*/
-          class="bg-[#6F6F6F] hover:bg-[#8CA99D] text-base font-bold py-5 px-16 border-b-4 border-[#A2FFD5] hover:border-[#80FFC6] rounded"> 
-            {list.header}
-          </Link>
-        )}
+    )
+  }
+  else {
+    return (
+      <div class="font-family: arial bg-[#464646] text-[#FFEFCC] border-t-8 border-t-[#54806C] pt-3 pb-1 shadow-[inset_0_-6px_8px_rgba(0,0,0,0.6)]" id="list-nav">
+        <div class="flex flex-row gap-4 justify-center">
+          {watchLists.map( list =>
+            <Link to={"../lists/" + username + "/" + listType + "/" + list.name} /*onClick={(e) => this.columnsDifferent(e, watchListData, list)}*/
+            class="bg-[#6F6F6F] hover:bg-[#8CA99D] text-base font-bold py-5 px-16 border-b-4 border-[#A2FFD5] hover:border-[#80FFC6] rounded"> 
+              {list.header}
+            </Link>
+          )}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
