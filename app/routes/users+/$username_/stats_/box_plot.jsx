6c6a97272fe97656246f8076435f003bfae619fe
ajccarlson <32660587+ajccarlson@users.noreply.github.com>
2024-04-22 16:21:24 -0700

    Added basic implementation of box plot to show average difference between objective and personal scores

---
+++ app/routes/users+/$username_/stats_/box_plot.jsx
@@ -0,0 +1,191 @@
+import { ResponsiveBoxPlot } from '@nivo/boxplot'
+
+function MyResponsiveBoxPlot(data) {
+  return (
+    <div class="user-landing-stats-box-plot-chart">
+      <ResponsiveBoxPlot
+          data={data}
+          margin={{ top: 60, right: 140, bottom: 60, left: 60 }}
+          minValue={0}
+          maxValue={10}
+          subGroupBy="group"
+          padding={0.12}
+          enableGridX={true}
+          axisTop={{
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: '',
+              legendOffset: 36,
+              truncateTickAt: 0
+          }}
+          axisRight={{
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: '',
+              legendOffset: 0,
+              truncateTickAt: 0
+          }}
+          axisBottom={{
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: 'group',
+              legendPosition: 'middle',
+              legendOffset: 32,
+              truncateTickAt: 0
+          }}
+          axisLeft={{
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: 'value',
+              legendPosition: 'middle',
+              legendOffset: -40,
+              truncateTickAt: 0
+          }}
+          colors={{ scheme: 'nivo' }}
+          colorBy="group"
+          borderRadius={2}
+          borderWidth={2}
+          borderColor={{
+              from: 'color',
+              modifiers: [
+                  [
+                      'darker',
+                      0.3
+                  ]
+              ]
+          }}
+          medianWidth={2}
+          medianColor={{
+              from: 'color',
+              modifiers: [
+                  [
+                      'darker',
+                      0.3
+                  ]
+              ]
+          }}
+          whiskerEndSize={0.6}
+          whiskerColor={{
+              from: 'color',
+              modifiers: [
+                  [
+                      'darker',
+                      0.3
+                  ]
+              ]
+          }}
+          motionConfig="stiff"
+          tooltip={(point) => {
+            console.log(point)
+            return (
+              <div
+                style={{
+                  background: 'black',
+                  color: point.color,
+                  padding: '9px 12px',
+                  border: '1px solid #ccc',
+                }}
+              >
+                <div>{`${point.group} (${point.data.n})`}</div>
+                <div>{`Mean: ${Number(point.formatted.mean).toFixed(1)}`}</div>
+                <div>{`Min: ${point.data.extrema[0]}`}</div>
+                <div>{`Max: ${point.data.extrema[1]}`}</div>
+              </div>
+            )
+          }} 
+          legends={[
+          {
+            anchor: 'bottom-right',
+            direction: 'column',
+            justify: false,
+            translateX: 120,
+            translateY: 0,
+            itemsSpacing: 10,
+            itemWidth: 100,
+            itemHeight: 18,
+            itemTextColor: 'white',
+            itemDirection: 'left-to-right',
+            itemOpacity: 1,
+            symbolSize: 18,
+            symbolShape: 'square',
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemTextColor: '#66563d'
+                }
+              }
+            ]
+          }
+        ]}
+      />
+    </div>
+  )
+}
+
+export function renderBoxPlotChart(loaderData, chartType) {
+  let typedBoxes = []
+
+  if (chartType == "objective scores") {
+    Object.entries(loaderData.typedEntries).forEach(([key, value]) => {
+      let scoredBars = []
+      for (let i = 0; i < 10; i++) {
+        scoredBars[i] = {
+          value: (i + 1),
+          personal: []
+        }
+      }
+
+      let objectiveType
+      if (value[0]) {
+        if ("tmdbScore" in value[0]) {
+          objectiveType = "tmdbScore"
+        }
+        else if ("malScore" in value[0]) {
+          objectiveType = "malScore"
+        }
+      }
+      else {
+        return
+      }
+
+      if (!objectiveType) {
+        throw new Error ("No objective score type found!")
+      }
+
+      value.forEach(typedEntry => {
+        if ((!isNaN(typedEntry[objectiveType]) && !isNaN(typedEntry["personal"])) && ((typedEntry[objectiveType] >= 1 && typedEntry[objectiveType] <= 10)  && (typedEntry["personal"] >= 1 && typedEntry["personal"] <= 10))) {
+          scoredBars[Math.floor(typedEntry[objectiveType])]["personal"].push(typedEntry["personal"])
+        }
+      })
+
+      let scoresFormatted = []
+      for (const scoreBar of scoredBars) {
+        if (scoreBar.personal.length > 0) {
+          const dataLength = scoreBar.personal.length
+          const dataAverage = scoreBar.personal.reduce((a, b) => Number(a) + Number(b)) / dataLength
+          const stdDeviation = Math.sqrt(scoreBar.personal.map(x => Math.pow(x - dataAverage, 2)).reduce((a, b) => Number(a) + Number(b)) / dataLength)
+
+          for (const personalScore of scoreBar.personal) {
+            scoresFormatted.push({
+              group: scoreBar.value,
+              subgroup: scoreBar.value,
+              value: personalScore,
+              mu: dataAverage,
+              sd: stdDeviation,
+              n: dataLength,
+            })
+          }
+        }
+      }
+      
+      typedBoxes.push(MyResponsiveBoxPlot(scoresFormatted))
+    })
+  }
+
+  return (typedBoxes)
+}
--- app/routes/users+/$username_/stats_/index.jsx
+++ app/routes/users+/$username_/stats_/index.jsx
@@ -1,4 +1,5 @@
 import { renderBarChart } from '#app/routes/users+/$username_/stats_/bar.jsx'
+import { renderBoxPlotChart } from '#app/routes/users+/$username_/stats_/box_plot'
 import { renderChordChart } from '#app/routes/users+/$username_/stats_/chord.jsx'
 import { renderLineChart } from '#app/routes/users+/$username_/stats_/line.jsx'
 import { renderPieChart } from '#app/routes/users+/$username_/stats_/pie.jsx'
@@ -7,7 +8,13 @@ import { renderRadialBar } from '#app/routes/users+/$username_/stats_/radial_bar
 export function StatsData(loaderData) {
   return (
     <div className="user-landing-stats-container">
+      {renderPieChart(loaderData)}
       {renderBarChart(loaderData, "score")}
+      {renderBoxPlotChart(loaderData, "objective scores")}
+      {renderLineChart(loaderData, "release")}
+      {renderLineChart(loaderData, "watched")}
+      {renderChordChart(loaderData)}
+      {renderRadialBar(loaderData, "type")}
     </div>
   )
 }
