d9eaf65d622df60bcdffe25d3667d6beb3b56d14
ajccarlson <32660587+ajccarlson@users.noreply.github.com>
2024-04-22 14:38:15 -0700

    Created basic implementation of bar graph for score distribution

--- app/routes/users+/$username_/stats_/index.jsx
+++ app/routes/users+/$username_/stats_/index.jsx
@@ -1,16 +1,13 @@
+import { renderBarChart } from '#app/routes/users+/$username_/stats_/bar.jsx'
 import { renderChordChart } from '#app/routes/users+/$username_/stats_/chord.jsx'
 import { renderLineChart } from '#app/routes/users+/$username_/stats_/line.jsx'
 import { renderPieChart } from '#app/routes/users+/$username_/stats_/pie.jsx'
-import { renderRadialBar } from '#app/routes/users+/$username_/stats_/radial_bar'
+import { renderRadialBar } from '#app/routes/users+/$username_/stats_/radial_bar.jsx'
 
 export function StatsData(loaderData) {
   return (
     <div className="user-landing-stats-container">
-      {renderPieChart(loaderData)}
-      {renderLineChart(loaderData, "release")}
-      {renderLineChart(loaderData, "watched")}
-      {renderChordChart(loaderData)}
-      {renderRadialBar(loaderData, "type")}
+      {renderBarChart(loaderData, "score")}
     </div>
   )
 }
