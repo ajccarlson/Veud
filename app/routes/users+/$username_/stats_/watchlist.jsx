83894cd3f915a080ed9814c9c7e69e5687ae6fc3
ajccarlson <32660587+ajccarlson@users.noreply.github.com>
2024-05-03 04:24:41 -0700

    Added basic watchlist stats chart

--- app/routes/users+/$username_/stats_/watchlist.jsx
+++ app/routes/users+/$username_/stats_/watchlist.jsx
@@ -2,7 +2,16 @@ export function watchlistStats(loaderData, listType) {
   let scoredEntries = 0
   let listSum = 0
 
+  let typedLists = {}
+
   loaderData.typedEntries[listType.id].forEach(typedEntry => {
+    if (!typedLists[typedEntry.watchlistId] && typedLists[typedEntry.watchlistId] != 0) {
+      typedLists[typedEntry.watchlistId] = 1
+    }
+    else {
+      typedLists[typedEntry.watchlistId]++
+    }
+
     if (typedEntry.personal && typedEntry.personal > 0) {
       scoredEntries++
     }
@@ -12,12 +21,23 @@ export function watchlistStats(loaderData, listType) {
 
   let listAverage = listSum / scoredEntries
   if (isNaN(listAverage)) {
-    listAverage = 0
+    listAverage = "N/A"
   }
 
   return (
     <div class="user-landing-stats-chart-container user-landing-watchlist-chart">
       {`Average: ${listAverage}`}
+      <div>
+        {Object.entries(typedLists).map(([listKey, listValue]) => {
+          return (
+            <div>
+              <span>{loaderData.watchLists.find(watchList => watchList.id == listKey).header}</span>
+              <span>{listValue}</span>
+            </div>
+          )
+        })}
+      </div>
+      <span>{`Total Entries: ${loaderData.typedEntries[listType.id].length}`}</span>
     </div>
   )
 }
